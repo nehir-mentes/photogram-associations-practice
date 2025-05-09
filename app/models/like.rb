e770bea0e322a9cd7496fc786e39c35eaeef01c4
nehir-mentes <nehirmen.98@gmail.com>
2025-05-09 17:47:24 +0000

    like wip

--- app/models/like.rb
+++ app/models/like.rb
@@ -17,8 +17,8 @@ class Like < ApplicationRecord
   })
   ## Direct associations
 
-  
+  belongs_to(:fan, class_name:"User", foreign_key:"fan_id")
   # Like#fan: returns a row from the users table associated to this like by the fan_id column
-
+  belongs_to(:photo, class_name:"Photo", foreign_key:"photo_id")
   # Like#photo: returns a row from the photo table associated to this like by the photo_id column
 end
