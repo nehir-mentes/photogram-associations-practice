197967739f2739d374e3d3208bf5f7dc638fd396
nehir-mentes <nehirmen.98@gmail.com>
2025-05-09 18:25:25 +0000

    wip2

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
 
   has_many(:accepted_sent_follow_requests, -> { where(status: "accepted") }, class_name:"Follow_Request", foreign_key:"sender_id")
   # User#accepted_sent_follow_requests: returns rows from the follow requests table associated to this user by the sender_id column, where status is 'accepted'
-  has_many(:accepted_received_follow_requests, :conditions  => { :status => "accepted" }, class_name:"Follow_Request", foreign_key:"recipient_id")
+  has_many(:accepted_received_follow_requests, -> { where(status: "accepted") }, class_name:"Follow_Request", foreign_key:"recipient_id")
   # User#accepted_received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column, where status is 'accepted'
 
   ## Indirect associations
